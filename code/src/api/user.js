0d5036677ae0dbd218f4a1a1ba660703f1e3595a
CoMAP <101875225+CoMAP2025@users.noreply.github.com>
2025-11-25 15:36:08 +0800

    Delete code directory

--- code/src/api/user.js
+++
@@ -1,9 +0,0 @@
-// src/api/user.js
-import api from "./index"
-
-export const createUser = async ({ email, nickname }) => {
-  const response = await api.post("/user/create", { email, nickname }, {
-    headers: { "Content-Type": "application/json" }
-  })
-  return response.data
-}
